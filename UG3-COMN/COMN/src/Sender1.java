5cefbceba85d98827a0a969d1f9d544351cf1b2c
Milan P <pavlik.mil@gmail.com>
2014-02-06 18:35:43 +0000

    Mock of Sender 1

--- UG3-COMN/COMN/src/Sender1.java
+++ UG3-COMN/COMN/src/Sender1.java
@@ -1,12 +1,97 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/*
+ * Milan Pavlik s1115104
+ */
+
 
 public class Sender1 {
+	
+	private int PORT;
+	private String FILENAME;
+	private static int PACKET_SIZE = 1024;
+	
+	public Sender1(int port, File file) {
+		
+		
+		try {
+			rdt_send(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	private BufferedReader readFile(String filename) throws FileNotFoundException {
+		return new BufferedReader(new FileReader(filename));
+	}
+	
+	public boolean rdt_send(File file) throws FileNotFoundException {
+		FileInputStream fstream = new FileInputStream(file);
+		
+		long fsize = file.length();
+		int chunkCount = (int) Math.floor(fsize / (double) PACKET_SIZE);
+		
+		byte[] buffer = new byte[PACKET_SIZE];
+		
+		// Submit each chunk
+		for (int i = 0; i < chunkCount; i++) {
+			try {
+				fstream.read(buffer, 0, PACKET_SIZE);
+				byte[] packet = make_pkt(buffer);
+				
+				udt_send(packet);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println(String.format("Reading file failed at chunk %d", i));
+				return false;
+			}
+		}
 
-	/**
-	 * @param args
-	 */
+		return true;
+	}
+	
+	private byte[] make_pkt(byte[] chunk) {
+		return chunk;
+	}
+	
+	private boolean udt_send(byte[] data) {
+		System.out.println("Chunk size: " + data[0]);
+		return false;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Parse args
+		try {
+			int port = Integer.parseInt(args[0]);
+			String filename = args[1];
+			
+			// Initialize file 
+			File file = new File(filename);
+			
+			System.out.println("Starting Sender1.");
+			
+			// Start Sender
+			Sender1 sender = new Sender1(port, file);
+			
+			
+		} catch(IndexOutOfBoundsException e) {
+			System.err.println("Incorect arguments. Valid arguments are <Port> <FileName> [RetryTimeout] [WindowSize]");
+			return;
+		} 
+		
+		
 	}
 
 }
