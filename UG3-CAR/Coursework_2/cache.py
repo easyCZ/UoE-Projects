58f064913aaea1cbbd00342f55a3145d88fdd864
Milan P <pavlik.mil@gmail.com>
2014-03-15 18:37:00 +0000

    Implemented Directly Mapped Cache - 36% DONE

--- UG3-CAR/Coursework_2/cache.py
+++ UG3-CAR/Coursework_2/cache.py
@@ -2,7 +2,9 @@ import argparse
 import math
 from collections import namedtuple
 
-Address = namedtuple('Address', 'tag index offset')
+BinaryAddress = namedtuple('BinaryAddress', 'tag index offset')
+Instruction = namedtuple('Instruction', 'op address')
+CacheEntry = namedtuple('CacheEntry', 'tag data')
 
 
 class Cache(object):
@@ -17,25 +19,80 @@ class Cache(object):
 
     def address_map(self, address, tag_len, index_len, offset_len):
         bin_addr = self.address_to_bin(address)
-        print bin_addr
-        return Address(
+        assert len(bin_addr) == self.ADDRESS_LENGTH
+        return BinaryAddress(
             bin_addr[:tag_len],
             bin_addr[tag_len:tag_len + index_len],
             bin_addr[tag_len + index_len:tag_len + index_len + offset_len])
+
+    def parse(self, instruction):
+        return Instruction(*instruction.split())
 
 
 class DirectMapped(Cache):
 
     BLOCK_SIZE = 32
+    READ, WRITE = 'R', 'W'
 
     def __init__(self, filename, cache_size):
+        self.filename = filename
         self.size = 1024 * cache_size
-        self.offset_length = int(math.log(BLOCK_SIZE, 2))
-        self.index_length = int(math.log(self.size / BLOCK_SIZE), 2)
-        self.tag_length = BLOCK_SIZE - self.offset_length - self.index_length
+        self.cache_blocks = self.size / self.BLOCK_SIZE
+        self.offset_length = int(math.log(self.BLOCK_SIZE, 2))
+        self.index_length = int(math.log(self.cache_blocks, 2))
+        self.tag_length = self.ADDRESS_LENGTH - self.offset_length - self.index_length
 
-    def get_block(self, address, cache_size):
-        return address % cache_size
+        self.cache = [None] * self.cache_blocks
+
+    def get_block(self, address, cache_blocks_len):
+        return int(address) % cache_blocks_len
+
+    def write(self, bin_address, stats):
+        cache_pos = self.get_block(bin_address.index, self.cache_blocks)
+        if self.cache[cache_pos] is not None and self.cache[cache_pos].tag == bin_address.tag:
+                stats['w_hit'] += 1
+        else:
+            stats['w_miss'] += 1
+        self.cache[cache_pos] = CacheEntry(bin_address.tag, None)
+
+    def read(self, bin_address, stats):
+        cache_pos = self.get_block(bin_address.index, self.cache_blocks)
+        if self.cache[cache_pos] is not None and self.cache[cache_pos].tag == bin_address.tag:
+                stats['r_hit'] += 1
+        else:
+            stats['r_miss'] += 1
+        self.cache[cache_pos] = CacheEntry(bin_address.tag, None)
+
+    def simulate(self):
+        stats = {'r_miss': 0, 'w_miss': 0, 'r_hit': 0, 'w_hit': 0}
+        try:
+            with open(self.filename) as _file:
+                for line in _file:
+                    instruction = self.parse(line)
+                    bin_addr = self.address_map(
+                        instruction.address,
+                        self.tag_length, self.index_length, self.offset_length)
+
+                    if instruction.op == self.WRITE:
+                        self.write(bin_addr, stats)
+                    else:
+                        self.read(bin_addr, stats)
+
+            self.display_stats(stats)
+
+        except IOError:
+            print("File '%s' could not be found." % self.filename)
+
+    def display_stats(self, stats):
+        total_miss_rate = 100 * (stats['r_miss'] + stats['w_miss']) / float(stats['total'])
+        read_miss_rate = 100 * stats['r_miss'] / float(stats['r_miss'] + stats['r_hit'])
+        write_miss_rate = 100 * stats['w_miss'] / float(stats['w_miss'] + stats['w_hit'])
+
+        print 'Total miss rate: %.6f' % total_miss_rate
+        print 'Read miss rate: %.6f' % read_miss_rate
+        print 'Write miss rate: %.6f' % write_miss_rate
+
+
 
 
 # if __name__ == "__main__":
@@ -50,6 +107,8 @@ class DirectMapped(Cache):
 #         # a.analyze(args.filename, length)
 #     except ValueError, e:
 #         print 'Cache Size needs to be an integer.'
-
-c = Cache()
-print c.address_map('1682318', 20, 7, 5)
+#
+c = DirectMapped('gcc_memref.out', 4)
+c.simulate()
+# print(c.cache_blocks)
+# print c.address_map('1682318', 36, 7, 5)
