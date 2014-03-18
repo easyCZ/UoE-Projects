ea00fae6f1a20e2a68892466f37fd77c2fc5dcd3
Milan P <pavlik.mil@gmail.com>
2014-03-18 16:58:11 +0000

    CAR Implementation done

--- UG3-CAR/Coursework_2/cache.py
+++ UG3-CAR/Coursework_2/cache.py
@@ -14,12 +14,21 @@ class Cache(object):
     READ, WRITE = 'R', 'W'
 
     def address_to_int(self, address):
+        """
+        Convert an address to hexadecimal.
+        """
         return int(address, 16)
 
     def address_to_bin(self, address):
+        """
+        Convert an address to binary, left padded to match ADDRESS_LENGTH.
+        """
         return bin(int(address, 16))[2:].zfill(self.ADDRESS_LENGTH)
 
     def address_map(self, address, tag_len, index_len, offset_len):
+        """
+        Split an address in binary into tag, index and offset.
+        """
         bin_addr = self.address_to_bin(address)
         assert len(bin_addr) == self.ADDRESS_LENGTH
         return BinaryAddress(
@@ -28,12 +37,21 @@ class Cache(object):
             bin_addr[tag_len + index_len:tag_len + index_len + offset_len])
 
     def parse(self, instruction):
+        """
+        Extract action and address from a line of the file.
+        """
         return Instruction(*instruction.split())
 
     def get_block(self, address, cache_blocks_len):
-        return int(address) % cache_blocks_len
+        """
+        Map an address to a block.
+        """
+        return int(address, 2) % cache_blocks_len
 
     def display_stats(self, stats):
+        """
+        Display statistics about the simulation.
+        """
         total = stats['r_miss'] + stats['w_miss'] + stats['r_hit'] + stats['w_hit']
         total_miss_rate = 100 * (stats['r_miss'] + stats['w_miss']) / float(total)
         read_miss_rate = 100 * stats['r_miss'] / float(stats['r_miss'] + stats['r_hit'])
@@ -45,52 +63,11 @@ class Cache(object):
 
 
 class DirectMapped(Cache):
-
-    def __init__(self, filename, cache_size):
-        self.filename = filename
-        self.size = 1024 * cache_size
-        self.cache_blocks = self.size / self.BLOCK_SIZE
-        self.offset_length = int(math.log(self.BLOCK_SIZE, 2))
-        self.index_length = int(math.log(self.cache_blocks, 2))
-        self.tag_length = self.ADDRESS_LENGTH - self.offset_length - self.index_length
-
-        self.cache = [None] * self.cache_blocks
-
-    def write(self, bin_address, stats):
-        cache_pos = self.get_block(bin_address.index, self.cache_blocks)
-        if self.cache[cache_pos] is not None and self.cache[cache_pos].tag == bin_address.tag:
-                stats['w_hit'] += 1
-        else:
-            stats['w_miss'] += 1
-        self.cache[cache_pos] = CacheEntry(bin_address.tag, None)
-
-    def read(self, bin_address, stats):
-        cache_pos = self.get_block(bin_address.index, self.cache_blocks)
-        if self.cache[cache_pos] is not None and self.cache[cache_pos].tag == bin_address.tag:
-                stats['r_hit'] += 1
-        else:
-            stats['r_miss'] += 1
-        self.cache[cache_pos] = CacheEntry(bin_address.tag, None)
-
-    def simulate(self):
-        stats = {'r_miss': 0, 'w_miss': 0, 'r_hit': 0, 'w_hit': 0}
-        try:
-            with open(self.filename) as _file:
-                for line in _file:
-                    instruction = self.parse(line)
-                    bin_addr = self.address_map(
-                        instruction.address,
-                        self.tag_length, self.index_length, self.offset_length)
-
-                    if instruction.op == self.WRITE:
-                        self.write(bin_addr, stats)
-                    else:
-                        self.read(bin_addr, stats)
-
-            self.display_stats(stats)
-
-        except IOError:
-            print("File '%s' could not be found." % self.filename)
+    """
+    Direct mapped cache is the same as Set associative where the associativity is 1.
+    Using the SetAssociative implementation instead.
+    """
+    pass
 
 
 class SetAssociative(Cache):
@@ -100,31 +77,37 @@ class SetAssociative(Cache):
         self.size = 1024 * cache_size     # Size in bytes
         self.cache_blocks = self.size / self.BLOCK_SIZE
         self.cache_sets = self.cache_blocks / associativity
+
         self.associativity = associativity
+
+        # Calculate the size of offset, index and tag
         self.offset_length = int(math.log(self.BLOCK_SIZE, 2))
-        self.index_length = int(math.floor(math.log(self.cache_blocks, 2)))
+        self.index_length = int(math.ceil(math.log(self.cache_sets, 2)))
         self.tag_length = self.ADDRESS_LENGTH - self.offset_length - self.index_length
 
-        self.cache = [deque(maxlen=associativity)] * self.cache_sets
-
-        print('Cache size: %d B' % self.size)
-        print('Cache blocks: %d' % self.cache_blocks)
-        print('Cache sets: %d' % self.cache_sets)
-        print('Tag: %d bits, Index: %d bits, Offset: %d bits' % (self.tag_length, self.index_length, self.offset_length))
+        # Use deque for each set, automatically losing elements as more data is appended
+        self.cache = [deque(maxlen=associativity) for i in range(self.cache_sets)]
 
     def read_write(self, bin_address, op, stats):
+        """
+        Perform READ/WRITE operation. Updates the cache.
+        """
         hit, miss = ('r_hit', 'r_miss') if op == self.READ else ('w_hit', 'w_miss')
         cache_block = self.get_block(bin_address.index, self.cache_sets)
+
         # Remove the entry if exists
         try:
-            self.cache[cache_block].remove(CacheEntry(bin_address.tag, None))
+            self.cache[cache_block].remove(bin_address.tag)
             stats[hit] += 1
         except ValueError:
             stats[miss] += 1
 
-        self.cache[cache_block].appendleft(CacheEntry(bin_address.tag, None))
+        self.cache[cache_block].append(bin_address.tag)
 
     def simulate(self):
+        """
+        Main entry of the simulation.
+        """
         stats = {'r_miss': 0, 'w_miss': 0, 'r_hit': 0, 'w_hit': 0}
         try:
             with open(self.filename) as _file:
@@ -142,28 +125,29 @@ class SetAssociative(Cache):
             print("File '%s' could not be found." % self.filename)
 
 
+if __name__ == '__main__':
+    valid_models = ['set_associative', 'direct_mapped']
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model', help='Which cache model to use.', type=str, choices=valid_models)
+    parser.add_argument('filename', help="The file to be used for simulation.", type=str)
+    parser.add_argument('cache_size', help="Size of the cache in KB", type=int)
+    parser.add_argument('-sets', help="The size of the set", type=int)
 
+    args = parser.parse_args()
 
+    if args.sets is None:
+        args.sets = 1
 
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('filename', help='File to analyze by the predictor.')
-#     parser.add_argument('cache_size', help='Size of the cache to model.')
-
-#     args = parser.parse_args()
-#     try:
-#         cache_size = int(args.cache_size)
-#         # a = AdaptativePredictor()
-#         # a.analyze(args.filename, length)
-#     except ValueError, e:
-#         print 'Cache Size needs to be an integer.'
-#
-c = SetAssociative('gcc_memref.out', 8, 2)
-c.simulate()
-#
-# d = DirectMapped('gcc_memref.out', 16)
-# d.simulate()
-# print(c.cache_blocks)
-# print c.address_map('1682318', 36, 7, 5)
+    try:
+        if args.model in valid_models:
+            model = args.model
+            if model == 'direct_mapped':
+                c = SetAssociative(args.filename, int(args.cache_size), 1)
+            if model == 'set_associative':
+                c = SetAssociative(args.filename, int(args.cache_size), int(args.sets))
+            c.simulate()
+        else:
+            print 'Valid options for cache model are', valid_models
+    except ValueError, e:
+        print 'cache_size and set_size need to be integers.'
