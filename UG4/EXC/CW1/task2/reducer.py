f3cca380dea458e0556079f87ed07553d24eaf87
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-11 23:53:48 +0100

    Add reducer error messages

--- UG4/EXC/CW1/task2/reducer.py
+++ UG4/EXC/CW1/task2/reducer.py
@@ -2,20 +2,27 @@
 
 import sys
 
-key = ""
-sentence = ""
-previous_key = ""
+try:
+    key = ""
+    sentence = ""
+    previous_key = ""
 
-for line in sys.stdin:
-    sentence = line.strip()
-    key, sentence = sentence.split('\t', 1)
+    for line in sys.stdin:
+        sentence = line.strip()
+        key, sentence = sentence.split('\t', 1)
+
+        print key, sentence
+
+        if previous_key != key:
+            if previous_key:
+                print(sentence)
+
+            previous_key = key
+
 
     if previous_key != key:
-        if previous_key:
-            print(sentence)
+        print(sentence)
 
-        previous_key = key
-
-
-if previous_key != key:
-    print(sentence)
+except Exception, e:
+    sys.stderr.write(e)
+    sys.exit(1)
