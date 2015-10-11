394b048e99536083fe746f3d4dda360929aa3d5f
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-12 00:19:20 +0100

    Check for none key?

--- UG4/EXC/CW1/task2/reducer.py
+++ UG4/EXC/CW1/task2/reducer.py
@@ -2,27 +2,21 @@
 
 import sys
 
-try:
-    key = ""
-    sentence = ""
-    previous_key = ""
 
-    for line in sys.stdin:
-        sentence = line.strip()
-        key, sentence = sentence.split('\t', 1)
+key = None
+sentence = ""
+previous_key = ""
 
-        print key, sentence
-
-        if previous_key != key:
-            if previous_key:
-                print(sentence)
-
-            previous_key = key
-
+for line in sys.stdin:
+    sentence = line.strip()
+    key, sentence = sentence.split('\t', 1)
 
     if previous_key != key:
-        print(sentence)
+        if previous_key:
+            print(sentence)
 
-except Exception, e:
-    sys.stderr.write(e)
-    sys.exit(1)
+        previous_key = key
+
+
+if key is not None and previous_key != key:
+    print(sentence)
