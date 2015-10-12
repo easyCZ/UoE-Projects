3ddc1e9a2a4c5024b9444c795aeb8806b9cf314c
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-12 11:15:15 +0100

    Handle empty lines

--- UG4/EXC/CW1/task2/reducer.py
+++ UG4/EXC/CW1/task2/reducer.py
@@ -9,7 +9,11 @@ previous_key = ""
 
 for line in sys.stdin:
     sentence = line.strip()
-    key, sentence = sentence.split('\t', 1)
+
+
+    tokens = sentence.split('\t')
+    key = tokens[0]
+    sentence = "\t".join(tokens[1:])
 
     if previous_key != key:
         if previous_key:
