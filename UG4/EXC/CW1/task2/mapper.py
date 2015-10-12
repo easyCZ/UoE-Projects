1031431cfe84ab559ae07df868c839b65e405c46
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-12 10:58:25 +0100

    Ignore empty lines

--- UG4/EXC/CW1/task2/mapper.py
+++ UG4/EXC/CW1/task2/mapper.py
@@ -6,5 +6,6 @@ import hashlib
 for line in sys.stdin:
     line = line.strip()
 
-    key = hashlib.md5(line).hexdigest()
-    print("{0}\t{1}".format(key, line))
+    if line:
+        key = hashlib.md5(line).hexdigest()
+        print("{0}\t{1}".format(key, line))
