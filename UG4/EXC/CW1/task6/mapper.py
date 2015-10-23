7847bbea6c388e829e222964921c1e7ed1bc4d1f
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-23 12:14:11 +0100

    Increase threshold to 20

--- UG4/EXC/CW1/task6/mapper.py
+++ UG4/EXC/CW1/task6/mapper.py
@@ -3,17 +3,18 @@ import sys
 import heapq
 
 most_common = []
+MOST_COMMON_THRESHOLD = 20
 heapq.heapify(most_common)
 
 for line in sys.stdin:
     count, word_pair = line.strip().split('\t', 1)
     count = int(count)
 
-    if len(most_common) >= 10:
+    if len(most_common) >= MOST_COMMON_THRESHOLD:
         heapq.heappushpop(most_common, (count, word_pair))
     else:
         heapq.heappush(most_common, (count, word_pair))
 
 
-for (count, word_pair) in heapq.nlargest(10, most_common):
+for (count, word_pair) in heapq.nlargest(MOST_COMMON_THRESHOLD, most_common):
     print("{0}\t{1}".format(count, word_pair))
