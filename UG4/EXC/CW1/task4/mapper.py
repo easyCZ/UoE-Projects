f3bbb846b1c528f2421af5a64b2146ce380f4209
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-25 00:22:57 +0100

    Task4 scripts

--- UG4/EXC/CW1/task4/mapper.py
+++ UG4/EXC/CW1/task4/mapper.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
-
+# mapper.py
 import sys
+from collections import defaultdict
 
 for line in sys.stdin:
     tokens = line.strip().split()
-    mapping = {}
+    mapping = defaultdict(list)
     pairs = zip(tokens[:-1], tokens[1:])
 
     for a, b in pairs:
-        if a in mapping:
-            mapping[a].append(b)
-        else:
-            mapping[a] = [b]
+        mapping[a].append(b)
 
     for (key, values) in mapping.iteritems():
         print("{0}\t{1}".format(key, ",".join(values)))
