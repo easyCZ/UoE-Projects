0af77352dfc5c30871e55e534113b072663c0969
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-24 15:07:56 +0100

    task 1 scripts

--- UG4/EXC/CW1/task1/mapper.py
+++ UG4/EXC/CW1/task1/mapper.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
 import sys
 
-
-for line in sys.stdin:
-    # Need to strip away newline chars
-    print(line.strip().lower())
+for line in sys.stdin: print(line.strip().lower())
