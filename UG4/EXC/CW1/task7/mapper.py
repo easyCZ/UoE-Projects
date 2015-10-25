a78dc8a7097381637803d4ab8cf1aa37882ef4c5
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-25 01:52:06 +0100

    Task 8

--- UG4/EXC/CW1/task7/mapper.py
+++ UG4/EXC/CW1/task7/mapper.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 # mapper.py
 import sys
-import os
 
-print "TaskCount:", os.environ["mapred.reduce.tasks"]
+for line in sys.stdin:
+    row, values = line.strip().split('\t')
+    row_values = values.split('  ')
 
-# for line in sys.stdin:
-#     row, values = line.strip().split('\t')
-#     row_values = values.split('  ')
-
-#     for (col, col_value) in enumerate(row_values):
-#         # out: <col>    <row>   <value>
-#         print("{0}\t{1}\t{2}".format(col, row, col_value))
+    for (col, col_value) in enumerate(row_values):
+        # out: <col>    <row>   <value>
+        print("{0}\t{1}\t{2}".format(col, row, col_value))
