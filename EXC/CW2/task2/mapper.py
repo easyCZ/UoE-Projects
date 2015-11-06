3b3e48a1dc068debb8430e0627f4bbb17a096cc4
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-06 18:52:48 +0000

    Experiment

--- EXC/CW2/task2/mapper.py
+++ EXC/CW2/task2/mapper.py
@@ -2,14 +2,18 @@
 # mapper.py
 import os
 import sys
-from collections import Counter
 
-filename = os.environ['mapreduce_map_input_file'].split('/')[-1]
-
+INPUT_FILES = os.environ['mapred.input.dir']
+# Input pattern:
+# "And : 151 : {(d1.txt, 34), (d10.txt, 4), (d12.txt, 25), (d13.txt, 5), (d15.txt, 19), (d2.txt, 17), (d3.txt, 34), (d4.txt, 1), (d5.txt, 4), (d6.txt, 1), (d7.txt, 1), (d9.txt, 6)}
 for line in sys.stdin:
-    words = line.strip().split()
-    if words:
-        counter = Counter(words)
+    print(INPUT_FILES)
+    # word, total_count, documents = line.strip().split(' : ', 2)
+    # documents = documents[1:-1].split(', ')
+    # documents = []
 
-        for (word, count) in counter.iteritems():
-            print("{0} {1} {2}".format(word, filename, count))
+    # if words:
+    #     counter = Counter(words)
+
+    #     for (word, count) in counter.iteritems():
+    #         print("{0} {1} {2}".format(word, filename, count))
