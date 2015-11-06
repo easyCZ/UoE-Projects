88bef34e9277a3b313f99fefe6178455f4211847
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-06 19:01:32 +0000

    print environ

--- EXC/CW2/task2/mapper.py
+++ EXC/CW2/task2/mapper.py
@@ -3,12 +3,13 @@
 import os
 import sys
 
-INPUT_FILES = os.environ['mapreduce_input_fileinputformat_inputdir']
-mapreduce.task.tmp.dir
+print(os.environ)
+# INPUT_FILES = os.environ['mapreduce_input_fileinputformat_inputdir']
+# mapreduce.task.tmp.dir
 # Input pattern:
 # "And : 151 : {(d1.txt, 34), (d10.txt, 4), (d12.txt, 25), (d13.txt, 5), (d15.txt, 19), (d2.txt, 17), (d3.txt, 34), (d4.txt, 1), (d5.txt, 4), (d6.txt, 1), (d7.txt, 1), (d9.txt, 6)}
-for line in sys.stdin:
-    print(INPUT_FILES)
+# for line in sys.stdin:
+#     print(INPUT_FILES)
     # word, total_count, documents = line.strip().split(' : ', 2)
     # documents = documents[1:-1].split(', ')
     # documents = []
