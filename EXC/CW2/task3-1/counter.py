b8f36a368f9d3f2572477fab8612867a8d5003d1
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-07 23:02:12 +0000

    implement 3-1

---
+++ EXC/CW2/task3-1/counter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# reducer.py
+import os
+import sys
+from collections import namedtuple
+
+MaxCount = namedtuple('MaxCount', ['resource', 'count'])
+
+max_counter = None
+resource_counter = 0
+last_resource = None
+
+
+for line in sys.stdin:
+    resource, count = line.strip().split('\t', 1)
+    count = int(count)
+
+    if not max_counter:
+        max_counter = MaxCount(resource, count)
+
+
+    if last_resource is not None and resource != last_resource:
+        # We received a new resource
+        if resource_counter > max_counter.count:
+            # We found a new max
+            max_counter = MaxCount(resource, resource_counter)
+        resource_counter = 0
+
+    last_resource = resource
+    resource_counter += count
+
+if max_counter:
+    print("{0}\t{1}".format(max_counter.resource, max_counter.count))
--- EXC/CW2/task3-1/mapper.py
+++ EXC/CW2/task3-1/mapper.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
 # mapper.py
-import os
 import sys
-from collections import Counter
 
-filename = os.environ['mapreduce_map_input_file'].split('/')[-1]
-
+# Input:
+# in24.inetnebr.com - - [01/Aug/1995:00:00:01 -0400] "GET /shuttle/missions/sts-68/news/sts-68-mcc-05.txt HTTP/1.0" 200 1839
 for line in sys.stdin:
-    words = line.strip().split()
-    if words:
-        counter = Counter(words)
-
-        for (word, count) in counter.iteritems():
-            print("{0} {1} {2}".format(word, filename, count))
+    try:
+        resource = line.strip().split('"')[1].split()[1]
+        print("{0}\t{1}".format(resource, 1))
+    except:
+        sys.stderr.write("Failed to parse, skipping. Line: {0}\n".format(line.strip()))
