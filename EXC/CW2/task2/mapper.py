3007e015ddc8207259e12df86c250bc5aec77ebd
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-07 16:48:39 +0000

    Chaing two jobs

--- EXC/CW2/task2/mapper.py
+++ EXC/CW2/task2/mapper.py
@@ -2,9 +2,11 @@
 # mapper.py
 import os
 import sys
+import math
 
 # Requires the number of files to map to be part of the input
-FILE_COUNT = int(sys.argv[1])
+DOCUMENT_COUNT = int(sys.argv[1])
+FILENAME = sys.argv[2]
 TERMS = set()
 
 # Build dictionary
@@ -17,15 +19,18 @@ with open('terms.txt') as f:
 # Input pattern:
 # "And : 151 : {(d1.txt, 34), (d10.txt, 4), (d12.txt, 25), (d13.txt, 5), (d15.txt, 19), (d2.txt, 17)}
 for line in sys.stdin:
-    pass
+    word, total_count, documents = line.strip().split(' : ', 2)
 
-print(TERMS)
-    # word, total_count, documents = line.strip().split(' : ', 2)
-    # documents = documents[1:-1].split(', ')
-    # documents = []
+    # We only care about the items in our dictionary
+    if word in TERMS:
+        # Serialize the freqencies into workable format
+        documents = [document.replace('(', '').replace(')', '') for document in documents[1:-1].split('), ')]
+        documents = [tuple(document.split(', ')) for document in documents]
 
-    # if words:
-    #     counter = Counter(words)
+        # Make documents a dict for instant lookup
+        documents = dict([(document, int(count)) for document, count in documents])
 
-    #     for (word, count) in counter.iteritems():
-    #         print("{0} {1} {2}".format(word, filename, count))
+        tf = documents.get(FILENAME, 0)
+        idf = math.log(DOCUMENT_COUNT / (1.0 + len(documents)), 10)
+        tf_idf = tf * idf
+        print("{0}, {1} = {2}".format(word, FILENAME, tf_idf))
