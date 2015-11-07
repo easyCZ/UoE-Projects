bab68468913d4eb4d6130dcd9709794d1a939952
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-07 15:42:25 +0000

    print file count

--- EXC/CW2/task2/mapper.py
+++ EXC/CW2/task2/mapper.py
@@ -4,15 +4,15 @@ import os
 import sys
 
 FILE_COUNT = int(sys.argv[1])
-print FILE_COUNT
 
 # print(os.environ)
 # INPUT_FILES = os.environ['mapreduce_input_fileinputformat_inputdir']
 # mapreduce.task.tmp.dir
 # Input pattern:
 # "And : 151 : {(d1.txt, 34), (d10.txt, 4), (d12.txt, 25), (d13.txt, 5), (d15.txt, 19), (d2.txt, 17), (d3.txt, 34), (d4.txt, 1), (d5.txt, 4), (d6.txt, 1), (d7.txt, 1), (d9.txt, 6)}
-# for line in sys.stdin:
-#     print(INPUT_FILES)
+
+for line in sys.stdin:
+    print(FILE_COUNT)
     # word, total_count, documents = line.strip().split(' : ', 2)
     # documents = documents[1:-1].split(', ')
     # documents = []
